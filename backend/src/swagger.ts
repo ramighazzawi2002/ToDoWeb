9d56a6a15ba90249e4a5319bb20b55ff34df3785
ramighazzawi2002 <rami.ghazzawiabed@gmail.com>
2025-07-12 01:00:43 +0300

    fix swagger

--- backend/src/swagger.ts
+++ backend/src/swagger.ts
@@ -1,5 +1,7 @@
 import swaggerAutogen from "swagger-autogen";
-
+import dotenv from "dotenv";
+dotenv.config();
+console.log("process.env.NODE_ENV:", process.env.NODE_ENV);
 const doc = {
   info: {
     title: "To-Do Backend API",
